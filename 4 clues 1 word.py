44ee45f6ae7513be75295f5f09b6c6093ef07e43
Benedict <141712326+bennybun29@users.noreply.github.com>
2023-12-01 09:41:12 +0800

    Update 4 clues 1 word.py

--- 4 clues 1 word.py
+++ 4 clues 1 word.py
@@ -16,6 +16,12 @@ pygame.mixer.music.play(-1)
 WIDTH, HEIGHT = 800, 600
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+ORANGE = (191, 91, 4)
+PINK = (205, 126, 89)
+SEAFOAM = (69, 115, 115)
+SUNFLOWER = (221, 178, 71)
+STORM = (60, 75, 77)
+RED = (255, 0, 0)
 
 # Words and Clues
 word_clue_pairs = [
@@ -85,7 +91,7 @@ score_tasks = {
     270: "Do your best impression of a famous person",
     280: "Send a funny GIF to a friend",
     290: "Create a funny nickname for yourself and use it for a day",
-    300: "Record a short video of yourself doing a funny talent",
+    300: "I DARE YOU TO DARE SOMEONE",
     # Add more score-task pairs as needed
 }
 
@@ -98,20 +104,15 @@ game_logo = pygame.image.load(r"C:\Users\user\Downloads\logo.png")
 game_logo = pygame.transform.scale(game_logo, (WIDTH, HEIGHT))
 game_logo_rect = game_logo.get_rect(topleft=(0, 0))
 
-# Background
-game_bg = pygame.image.load(r"C:\Users\user\Downloads\background.png")
-game_bg = pygame.transform.scale(game_bg, (WIDTH, HEIGHT))
-game_bg_rect = game_bg.get_rect(topleft=(0, 0))
-
 # Finish Background
-finish_bg = pygame.image.load(r"C:\Users\user\Downloads\FINISH.png")
+finish_bg = pygame.image.load(r"C:\Users\user\Downloads\finish_bg.png")
 finish_bg = pygame.transform.scale(finish_bg, (WIDTH, HEIGHT))
 finish_bg_rect = finish_bg.get_rect(topleft=(0, 0))
 
 #Wrong Background
-wrong = pygame.image.load(r"C:\Users\user\Downloads\wrong.png")
-wrong = pygame.transform.scale(wrong, (WIDTH, HEIGHT))
-wrong_rect = wrong.get_rect(topleft=(0, 0))
+wrong_bg = pygame.image.load(r"C:\Users\user\Downloads\wrong_bg.png")
+wrong_bg = pygame.transform.scale(wrong_bg, (WIDTH, HEIGHT))
+wrong_bg_rect = wrong_bg.get_rect(topleft=(0, 0))
 
 #Clues Background
 clues_bg = pygame.image.load(r"C:\Users\user\Downloads\clues_bg.png")
@@ -146,7 +147,7 @@ def generate_word():
     word_clue_pairs_copy.remove(word_clue_pair)  # Remove the chosen word_clue_pair from the copy
 
 #Introductory Screen
-def draw_intro_screen():
+def intro_screen():
     screen.fill(WHITE)
 
     screen.blit(game_logo, game_logo_rect)
@@ -154,37 +155,37 @@ def draw_intro_screen():
     pygame.display.flip()
 
 #Displays and get the player's details
-def draw_screen():
+def playing_screen():
     screen.fill(WHITE)
 
     screen.blit(clues_bg, clues_bg_rect)
 
     clue_y = 160
     for clue in current_clues:
-        text = font.render(clue, True, BLACK)
+        text = font.render(clue, True, SEAFOAM)
         text_rect = text.get_rect(center=(WIDTH // 2, clue_y))
         screen.blit(text, text_rect)
         clue_y += 40
 
-    guess_text = input_font.render("Your Guess: " + current_guess, True, BLACK)
+    guess_text = input_font.render("Your Guess: " + current_guess, True, SUNFLOWER)
     guess_text_rect = guess_text.get_rect(center=(WIDTH // 2, 370))  # Adjusted the Y-position
     screen.blit(guess_text, guess_text_rect)
 
-    attempts_text = font.render(f"Attempts left: {attempts}", True, BLACK)
+    attempts_text = font.render(f"Attempts left: {attempts}", True, PINK)
     attempts_text_rect = attempts_text.get_rect(center=(WIDTH // 2, 470))  # Adjusted the Y-position
     screen.blit(attempts_text, attempts_text_rect)
 
-    score_text = font.render(f"Score: {score}", True, BLACK)
+    score_text = font.render(f"Score: {score}", True, STORM)
     score_text_rect = score_text.get_rect(center=(WIDTH // 2, 520))  # Adjusted the Y-position
     screen.blit(score_text, score_text_rect)
 
     pygame.display.flip()
 
 #Display a message when they got the correct answer
-def show_message(message, delay=2000):
+def show_correct_message(message, delay=2000):
     screen.fill(WHITE)
     screen.blit(correct_bg, correct_bg_rect)
-    text = font.render(message, True, BLACK)
+    text = font.render(message, True, ORANGE)
     text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
     screen.blit(text, text_rect)
     pygame.display.flip()
@@ -194,20 +195,21 @@ def show_message(message, delay=2000):
 def reveal_answer():
     delay = 10000
     screen.fill(WHITE)
-    screen.blit(wrong, wrong_rect)
+    screen.blit(wrong_bg, wrong_bg_rect)
     
-    answer_text = font.render(f"The correct answer is: {current_word}", True, BLACK)
+    answer_text = font.render(f"The correct answer is: {current_word}", True, PINK)
     answer_text_rect = answer_text.get_rect(center=(WIDTH // 2, HEIGHT // 5.3))  # Adjusted the Y-position
     screen.blit(answer_text, answer_text_rect)
-    end = font.render('YOU LOSE!', True, BLACK)
+    end = font.render('YOU LOSE!', True, RED)
     end_rect = end.get_rect(center=(WIDTH // 2, HEIGHT // 10))  # Adjusted the Y-position
     screen.blit(end, end_rect)
     
-    score_text = font.render(f"Your Score: {score}", True, BLACK)
+    score_text = font.render(f"Your Score: {score}", True, STORM)
     score_text_rect = score_text.get_rect(center=(WIDTH // 2, HEIGHT // 3.5))
     screen.blit(score_text, score_text_rect)
+
     task = score_tasks.get(score, "No task for this score")
-    task_text = font.render(f"TASK: {task}", True, BLACK)
+    task_text = font.render(f"TASK: {task}", True, STORM)
     task_text_rect = task_text.get_rect(center=(WIDTH // 2, HEIGHT // 1.2))
     screen.blit(task_text, task_text_rect)
     
@@ -225,17 +227,17 @@ def end_game():
     screen.blit(finish_bg, finish_bg_rect)
 
     # Display the message
-    text = font.render("You finished the GAME!", True, BLACK)
+    text = font.render("You FINISHED the GAME!", True, RED)
     text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 5))
     screen.blit(text, text_rect)
 
     # Display the score and associated task
-    score_text = font.render(f"Your Score: {score}", True, BLACK)
+    score_text = font.render(f"Your Score: {score}", True, STORM)
     score_text_rect = score_text.get_rect(center=(WIDTH // 2, HEIGHT // 3.3))
     screen.blit(score_text, score_text_rect)
 
     task = score_tasks.get(score, "No task for this score")
-    task_text = font.render(f"TASK: {task}", True, BLACK)
+    task_text = font.render(f"TASK: {task}", True, STORM)
     task_text_rect = task_text.get_rect(center=(WIDTH // 2, HEIGHT // 1.2))
     screen.blit(task_text, task_text_rect)
 
@@ -245,7 +247,9 @@ def end_game():
     pygame.quit()
     sys.exit()
 
-draw_intro_screen()
+
+# GAME INTRODUCTION!
+intro_screen()
 pygame.display.flip()
 
 # Wait for Enter key press to start the game
@@ -257,7 +261,7 @@ while play:
     pygame.time.delay(100)
 
 generate_word()
-draw_screen()
+playing_screen()
 
 # Game loop
 running = True
@@ -272,7 +276,7 @@ while running:
                 current_guess = current_guess[:-1]
             elif event.key == pygame.K_RETURN:
                 if current_guess.lower() == current_word:
-                    show_message("Correct answer!", delay=1000)
+                    show_correct_message("Correct answer!", delay=1000)
                     generate_word()
                     attempts = 3
                     score += 10
@@ -288,11 +292,11 @@ while running:
                         current_guess = ""
                 if attempts != 0:
                     current_guess = ""
-                draw_screen()
+                playing_screen()
             else:
                 current_guess += event.unicode
 
-    draw_screen()
+    playing_screen()
 
 # Quit Pygame
 pygame.mixer.music.stop()
